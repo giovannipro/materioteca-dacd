90ef38fa69b26443fa026c986f91dc377bb05d50
giovannipro <g.profet@gmail.com>
2022-04-13 00:31:55 +0200

    refine cover

--- assets/script/home.js
+++ assets/script/home.js
@@ -1,18 +1,18 @@
 const pics = [
-    "23338_Cristallina_White_quadro.jpg",
-    "23339_Cristallina_Pearl_Grey_quadro.jpg",
-    "23340_Cristallina_Ivory_quadro.jpg",
+    "23542_Cristallina_Violet_quadro.jpg",
+    "23540_Cristallina_Fantastico_quadro.jpg",
     "23341_Cristallina_Brown_quadro.jpg",
+    "23340_Cristallina_Ivory_quadro.jpg",
+    "23339_Cristallina_Pearl_Grey_quadro.jpg",
+    "23338_Cristallina_White_quadro.jpg",
     "23346_Borcatello_quadro.jpg",
+    "23543_Cristallina_Black_and_White_quadro.jpg",
     "23345_Rosso_dArzo_quadro.jpg",
     "23348_Macchiavecchia_gialla_quadro.jpg",
     "23350_Macchiavecchia_grigia_quadro.jpg",
     "23508_Macchiavecchia_rossa_quadro.jpg",
-    "23541_Cristallina_Green_quadro.jpg",
-    "23540_Cristallina_Fantastico_quadro.jpg",
-    "23542_Cristallina_Violet_quadro.jpg",
-    "23543_Cristallina_Black_and_White_quadro.jpg",
-    "23544_Cristallina_Arabescato_quadro.jpg"
+    "23544_Cristallina_Arabescato_quadro.jpg",
+    "23541_Cristallina_Green_quadro.jpg"
 ]
 
 function home() {
